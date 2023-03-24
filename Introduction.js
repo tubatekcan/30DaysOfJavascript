a5c6dba6bdfba7fb1ebcdc75d496607b3256a8e6
Tuba Tekcan <69577958+tubatekcan@users.noreply.github.com>
2023-03-25 01:26:51 +0300

    Add files via upload

---
+++ Introduction.js
@@ -0,0 +1,14 @@
+// console.log('bu yazı Introduction.JS ten geldi.')
+
+console.log(typeof'Improgramming')
+console.log(typeof"5")
+console.log(typeof 5)
+
+
+let youtubeChannel="Prototurk"
+let channelAge=10
+let isChannelStillActive=true
+let channelMoney=undefined
+const Pi=3.14
+const gravity=9.819
+console.log(Pi,gravity,channelMoney,channelAge)
